e5616406d59483c518f5d83430badbc4388d36b2
sanijo <sanijo333@gmail.com>
2023-05-28 20:00:06 +0200

    Added html code

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,5 +19,10 @@ func routes(app *config.AppConfig) http.Handler {
     mux.Get("/", handlers.Repo.Home)
     mux.Get("/about", handlers.Repo.About)
 
+    // In static folder are all things that are not html template such as JS,
+    // figures
+    filseServer := http.FileServer(http.Dir("./static/"))
+    mux.Handle("/static/*", http.StripPrefix("/static", filseServer))
+
     return mux
 } 
